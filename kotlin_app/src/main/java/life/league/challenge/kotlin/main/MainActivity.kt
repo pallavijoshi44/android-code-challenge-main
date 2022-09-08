466525e3c023d9374ab8a467c9b84541220ad605
Pallavi <pallavijoshi4@gmail.com>
2022-09-08 13:32:47 +0200

    Add  composable for displaying user list

--- kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
@@ -3,17 +3,12 @@ package life.league.challenge.kotlin.main
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.google.accompanist.appcompattheme.AppCompatTheme
+import life.league.challenge.kotlin.main.composable.UserList
+import life.league.challenge.kotlin.main.theme.PagingSampleTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -31,23 +26,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val usersList = viewModel.usersPager.collectAsLazyPagingItems()
-            AppCompatTheme {
-                MainScreen()
+            PagingSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    UserList(viewModel = viewModel)
+                }
             }
         }
-    }
-
-    @Composable
-    private fun MainScreen() {
-        Text(
-            text = "ABC",
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 5.dp)
-                .wrapContentWidth(Alignment.CenterHorizontally)
-        )
     }
 
     companion object {
---
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/main/composable/Users.kt
@@ -0,0 +1,175 @@
+package life.league.challenge.kotlin.main.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import life.league.challenge.kotlin.R
+import life.league.challenge.kotlin.main.MainActivityViewModel
+import life.league.challenge.kotlin.model.UserDetails
+
+
+@Composable
+fun UserList(viewModel: MainActivityViewModel) {
+
+    val usersList = viewModel.usersPager.collectAsLazyPagingItems()
+
+    LazyColumn {
+        items(usersList) { item ->
+            item?.let { UserCard(user = it) }
+        }
+
+        when (usersList.loadState.append) {
+            is LoadState.NotLoading -> Unit
+            LoadState.Loading -> {
+                item {
+                    LoadingItem()
+                }
+            }
+            is LoadState.Error -> {
+                item {
+                    ErrorItem(message = "Some error occurred")
+                }
+            }
+        }
+
+        when (usersList.loadState.refresh) {
+            is LoadState.NotLoading -> Unit
+            LoadState.Loading -> {
+                item {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+            is LoadState.Error -> TODO()
+        }
+    }
+}
+
+
+@Composable
+fun UserCard(user: UserDetails) {
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
+            .padding(6.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(user.avatar)
+                    .crossfade(true)
+                    .build(),
+                placeholder = painterResource(R.drawable.ic_avatar_placeholder),
+                contentDescription = stringResource(R.string.description),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .width(42.dp)
+                    .height(42.dp)
+            )
+            Text(
+                text = user.userName,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .align(CenterVertically)
+            )
+        }
+    }
+}
+
+@Composable
+fun LoadingItem() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        contentAlignment = Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(42.dp)
+                .height(42.dp)
+                .padding(8.dp),
+            strokeWidth = 5.dp
+        )
+
+    }
+}
+
+@Composable
+fun ErrorItem(message: String) {
+    Card(
+        elevation = 2.dp,
+        modifier = Modifier
+            .padding(6.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+                .padding(8.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .width(42.dp)
+                    .height(42.dp),
+                painter = painterResource(id = R.drawable.ic_error),
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+            Text(
+                color = Color.White,
+                text = message,
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .align(CenterVertically)
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun UserPreview() {
+    UserCard(user = UserDetails("Belal", "username", "title", "null"))
+}
